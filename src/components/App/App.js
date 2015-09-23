12a0a3e39ba65cecabd8b10aec22280782f9d9b5
DanielHabib <d.g.habib7@gmail.com>
2015-09-22 22:24:34 -0400

    update angle Calc

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,7 @@
-
-import React, { PropTypes } from 'react';
+import React, {
+    PropTypes
+}
+from 'react';
 
 import styles from './App.css';
 import withContext from '../../decorators/withContext';
@@ -12,28 +14,40 @@ import TeamNode from '../TeamNode';
 import MemberNode from '../MemberNode';
 import $ from 'jquery';
 import PubSub from 'pubsub-js';
+import { render } from 'react-dom';
 // import xmlhttp from 'xmlhttp';
 @withContext
 @withStyles(styles)
-class App extends React.Component{
+class App extends React.Component {
 
-  static propTypes = {
-    children: PropTypes.element.isRequired,
-    error: PropTypes.object
-  };
-  static defaultProps = {
-  }
-  constructor() {
-    super();
-      this.state = {
-        result: 0,
-        criteria: 'squad/',
-        children:[],
-        root: "Root",
-        currentNode:"node",
-        childDom: [],
-        treeRoot: "None"
-      };
+    static propTypes = {
+        children: PropTypes.element.isRequired,
+        error: PropTypes.object
+    };
+
+    static defaultProps = {
+        radius: 100
+    }
+
+    calculateAngle() {
+        var degreesOfSeparation = 360 / (self.this.childDom.length + 1);
+        return degreesOfSeparation;
+    }
+    calculatePosition(degreesOfSeparation, index) {
+
+        return [];
+
+    }
+    constructor() {
+        super();
+        this.state = {
+            result: 0,
+            criteria: 'squad/',
+            children: [{name:"Loading"}],
+            root: "Root",
+            currentNode: "node",
+            treeRoot: "None"
+        };
     };
 
     componentDidMount() {
@@ -42,98 +56,82 @@ class App extends React.Component{
         }.bind(this);
         PubSub.subscribe('text_pushed', puhsuh);
 
-    var url = "http://45.55.167.3:5000/".concat(this.state.criteria)
+        var url = "http://45.55.167.3:5000/".concat(this.state.criteria)
 
-    $.ajax({
-       type: 'GET',
-        url: url,
-        async: true,
-        dataType: 'json',
-        success: function(json) {
-           console.log("sucess");
-           this.onSuccess(json)
+        $.ajax({
+            type: 'GET',
+            url: url,
+            async: true,
+            dataType: 'json',
+            success: function(json) {
+                console.log("sucess");
+                this.onSuccess(json)
 
-        }.bind(this),
-        error: function(e) {
-          console.log("fail");
-          console.log(e.message);
+            }.bind(this),
+            error: function(e) {
+                console.log("fail");
+                console.log(e.message);
+            }
+        });
+    }
+
+    onSuccess(json) {
+        this.setState({
+            root: json.result,
+            treeRoot: json.result,
+            children: json.result.children
+        });
+    }
+
+    onClickChild(node) {
+        this.setState({
+            root: node,
+            children: node.children
+        });
+    }
+
+    backToTop() {
+        this.onClickChild(this.state.treeRoot)
+    }
+    displayDescription() {
+
+    }
+
+    displayChildren() {
+        console.log("display children");
+    }
+    renderItem(item){
+          return (
+          <div onClick={() => this.onClickChild(item)}>
+            <TeamNode name={item.name} desc={item.desc} />
+          </div>
+          )
         }
-    });
-  }
-
-
-  onSuccess(json) {
-    this.setState({
-      root: json.result,
-      treeRoot: json.result
-    });
-    this.buildChildren();
-  }
-
-  buildChildren() {
-
-    for (var index in this.state.childDom) {
-      var el = this.state.childDom[index];
-      el.remove();
+    renderChildren(items, node){
+      if (!node.leaf){
+        return (
+          <ul style={{listStyleType: "none"}}>
+              {items.map((item, index) =>
+            <li key={item.name}>
+            {this.renderItem(item)}
+            </li>)}
+          </ul>
+          )
+      }
     }
-    var element = document.getElementById("CenterNode");
-      this.setState({
-        children: this.state.root.children
-    });
-    for (var index in this.state.children) {
-      var node = this.state.root.children[index];
-      this.state.currentNode = node;
-      var val = node.name;
-      var desc = node.desc;
-      var child = document.createElement("p");
-      var text = document.createTextNode(val);
-      child.id = index;
 
-      child.onclick = function(node) {
-        this.onClickChild(node)
-
-      }.bind(this, node);
-
-      child.className='ChildNode';
-      child.appendChild(text);
-      element.appendChild(child);
-      this.state.childDom.push(child);
+    render() {
+      var items = this.state.children;
+      var node = this.state.root;
+        return !this.props.error ? (
+      <div id = "FullScreen">
+        <MemberNode onClick = {this.backToTop.bind(this)}/>
+        <TeamNode name={this.state.root.name} desc={this.state.root.desc}/>
+        {this.renderChildren(items, node)}
+      </div >
+        ) : this.props.children;
     }
-  }
-
-  onClickChild (node){
-    console.log("Child Clicked");
-    this.setState({
-      root: node
-          });
-    console.log(node);
-    this.buildChildren();
-  }
-
-  backToTop() {
-    console.log("back to top");
-    this.setState({
-      root: this.state.treeRoot,
-          });
-    this.buildChildren();
-  }
-  displayDescription() {
-
-  }
-
-  displayChildren() {
-    console.log("display children");
-  }
-
-  render() {
-    return !this.props.error ? (
-      <div id="FullScreen" onClick={this.displayChildren.bind(this)}>
-        <MemberNode onClick={this.backToTop.bind(this)} />
-        <TeamNode name={this.state.root.name} desc={this.state.root.desc} />
-        <p>{this.state.result}</p>
-      </div>
-    ) : this.props.children;
-  }
 }
+//         <pre fontSize='5px'> {JSON.stringify(this.state.children, null, 2)} </pre>
 
 export default App;
